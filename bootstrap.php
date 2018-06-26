47a7159ec8d1d9e427bd35679a110441bb19f3a6
JustinasPHP <justinas.straukas@gmail.com>
2018-06-26 20:10:39 +0300

    first commit

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JustinasPhp
+ * Date: 2018-06-26
+ * Time: 20:04
+ */
+
+define('ROOT_PATH', __DIR__);
+
+require_once ROOT_PATH . '/vendor/autoload.php';
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JustinasPhp
+ * Date: 2018-06-26
+ * Time: 19:57
+ */
+
+require_once 'bootstrap.php';
+
+use Hello\SayHello;
+
+echo SayHello::world();
